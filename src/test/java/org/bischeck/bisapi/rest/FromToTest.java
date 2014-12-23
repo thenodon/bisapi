a6913b5b94205dddb29c04fbbc433029e409a9c9
Anders Haal <anders.haal@ingby.com>
2014-12-23 12:31:38 +0100

    Method name change

--- src/test/java/org/bischeck/bisapi/rest/FromToTest.java
+++ src/test/java/org/bischeck/bisapi/rest/FromToTest.java
@@ -12,26 +12,26 @@ public class FromToTest {
 	public void offset() {
 		FromTo fromto = new FromTo(Optional.ofNullable("5"),
 				Optional.ofNullable("15"));
-		Assert.assertEquals(fromto.offset(), new Integer("11"));
+		Assert.assertEquals(fromto.hasOffset(), new Integer("11"));
 		fromto = new FromTo(Optional.ofNullable("0"), Optional.ofNullable("15"));
-		Assert.assertEquals(fromto.offset(), new Integer("16"));
+		Assert.assertEquals(fromto.hasOffset(), new Integer("16"));
 		fromto = new FromTo(Optional.ofNullable("0"),
 				Optional.ofNullable("+15"));
-		Assert.assertEquals(fromto.offset(), new Integer("15"));
+		Assert.assertEquals(fromto.hasOffset(), new Integer("15"));
 		fromto = new FromTo(Optional.ofNullable("0"),
 				Optional.ofNullable("  +15	"));
-		Assert.assertEquals(fromto.offset(), new Integer("15"));
+		Assert.assertEquals(fromto.hasOffset(), new Integer("15"));
 		fromto = new FromTo(Optional.ofNullable(" 0   "),
 				Optional.ofNullable("  +15	"));
-		Assert.assertEquals(fromto.offset(), new Integer("15"));
+		Assert.assertEquals(fromto.hasOffset(), new Integer("15"));
 		fromto = new FromTo(Optional.ofNullable(" 10   "),
 				Optional.ofNullable("  +5	"));
-		Assert.assertEquals(fromto.offset(), new Integer("5"));
+		Assert.assertEquals(fromto.hasOffset(), new Integer("5"));
 		Assert.assertEquals(fromto.getTo(), new Integer("14"));
 		Assert.assertEquals(fromto.getFrom(), new Integer("10"));
 
 		fromto = new FromTo(5, 15);
-		Assert.assertEquals(fromto.offset(), new Integer("11"));
+		Assert.assertEquals(fromto.hasOffset(), new Integer("11"));
 		Assert.assertEquals(fromto.getTo(), new Integer("15"));
 		Assert.assertEquals(fromto.getFrom(), new Integer("5"));
 	}
